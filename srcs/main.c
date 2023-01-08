94cfec42c5bd71bb6965f663eeb2cdd543d9d2e5
Mohamed M'rabet <mhmdmrabet@yahoo.fr>
2023-01-08 11:02:29 +0100

    First try bonus

--- srcs/main.c
+++ srcs/main.c
@@ -11,47 +11,10 @@
 /* ************************************************************************** */
 
 #include "./pipex.h"
-#include <time.h>
 
 int	main(int argc, char **argv, char **envp)
 {
-	pid_t	pid;
-	int		error;
-	int		wait_status;
-	int		status_code;
-	int		file;
-	int		i;
-
-	i = 0;
-	while (envp[i])
-	{
-		printf("%s\n", envp[i++]);
-	}
-
-	(void)argc;
-	pid = fork();
-	if (pid == -1)
-		return (1);
-	if (pid == 0)
-	{
-		file = open("result.txt", O_WRONLY | O_CREAT, 0777);
-		if (file == -1)
-			return (2);
-		error = execve("/usr/bin/ls", argv, envp);
-		if (error == -1)
-			return (printf("Could not find program to execute.\n"), 0);
-	}
-	else
-	{
-		wait(&wait_status);
-		if (WIFEXITED(wait_status))
-		{
-			status_code = WEXITSTATUS(wait_status);
-			if (status_code == 0)
-				printf("SUCCESS\n");
-			else
-				printf("Failure with status code %d\n", status_code);
-		}
-	}
+	if (argc < 5)
+		return (0);
 	return (0);
 }
